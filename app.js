f205cbc9473200125305ac1c8a31670aaabb3276
Alessandro Pecorilla <APecorillaLearning@Yahoo.com>
2023-12-24 15:40:07 +0100

    Implemente first server listen and first steps with http module

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('Express Tutorial')
+// Requiring the 'http' module
+const http = require('http');
+
+// Creating an HTTP server
+const server = http.createServer((req, res) => {
+    // Extracting the requested URL from the request
+    const url = req.url;
+
+    // Handling different routes based on the requested URL
+    if (url === '/') {
+        // Setting the response header with a 200 status code and content type
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        });
+        res.write('<h1>Home page</h1>'); // Sending the HTML content for the home page
+        res.end(); // Ending the response
+    }
+    // About page
+    else if (url === '/about') {
+        // Setting the response header with a 200 status code and content type
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        });
+        res.write('<h1>About page</h1>'); // Sending the HTML content for the about page
+        res.end(); // Ending the response
+    }
+    // Not found
+    else {
+        // Setting the response header with a 200 status code and content type
+        res.writeHead(200, {
+            'content-type': 'text/html'
+        });
+        res.write('<h1>Page not found</h1>'); // Sending the HTML content for the page not found
+        res.end(); // Ending the response
+    }
+});
+
+// Listening on port 6575
+server.listen(6575);
